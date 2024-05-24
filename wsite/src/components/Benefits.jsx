ff7671d505e6cc6187c5c4da6bf62b17466c5d1e
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-05-24 16:34:41 +0530

    Update Benefits.jsx

--- wsite/src/components/Benefits.jsx
+++ wsite/src/components/Benefits.jsx
@@ -26,14 +26,6 @@ const Benefits = () => {
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
-                  <img
-                    src={item.iconUrl}
-                    width={48}
-                    height={48}
-                    alt={item.title}
-                  />
-                </div>
               </div>
 
               {item.light && <GradientLight />}
