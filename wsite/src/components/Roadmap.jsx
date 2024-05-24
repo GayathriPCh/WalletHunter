df1b82bf561dc53d59bd6efba1e0012c20e496ea
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-05-24 16:11:13 +0530

    Update Roadmap.jsx

--- wsite/src/components/Roadmap.jsx
+++ wsite/src/components/Roadmap.jsx
@@ -30,7 +30,7 @@ const Roadmap = () => (
       </div>
 
       <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Our roadmap</Button>
+        <Button href="/roadmap">Back to top</Button>
       </div>
     </div>
   </Section>
